fbe310fd4b11e73345f4e83808820fbd75fd7984
fphoebe <fphoebe36@gmail.com>
2023-03-19 12:09:10 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 /**
   * main -entry point
-  *
+iiiiiiiiii  *
   * Return: Always 0 (success)
   */
 /* betty style doc for function main goes there */
@@ -23,9 +23,11 @@ int main(void)
 	{
 		printf("last digit of  and is zero\n", n);
 	}
-	else(n < 6 && n != 0)
+	else
+		(n < 6 && n != 0)
 	{
 		printf("last digit of %d and is less than 6 and 0");
 	}
 	return (0);
 }
+
