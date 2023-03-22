9726ac26b3b16d9528828f697df17615c2635b58
fphoebe <fphoebe36@gmail.com>
2023-03-22 16:08:48 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - Entry point of a program that prints _putchar followed by a new line
- * Return : Value is 0
+ * main - Program that prints _putchar, followed by a new line
+ * Return: Always 0
  */
 int main(void)
 {
