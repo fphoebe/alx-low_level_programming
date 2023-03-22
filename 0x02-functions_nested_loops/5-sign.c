3b3dd5cec565ca8a2b6ae51ea016b33d31d3717b
fphoebe <fphoebe36@gmail.com>
2023-03-22 14:55:42 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,32 +1,26 @@
-
 #include "main.h"
 
 /**
- * jack_bauer - A program that prints minute of the day
- * Return: void
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
-void jack_bauer(void)
+int print_sign(int n)
 {
-	int a, b, c, d;
-
-	for (a = 48; a <= 50; a++)
+	if (n > 0)
 	{
-		for (b = 48; b <= 57; b++)
-		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-					break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
-		}
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
 	}
+	return (-1);
 }
+
