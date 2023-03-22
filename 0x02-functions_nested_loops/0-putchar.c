741a6d2e8f152fbb69bcd2d208232f5cb2477415
fphoebe <fphoebe36@gmail.com>
2023-03-22 00:25:18 -0700

    putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *main -Entry point
+  *Description -prints -putchar to stdout
+  *Return -always 0
+   */
+int main(void)
+{
+	-putchar('-');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r'); 
+	-putchar('\n');
+	return(0);
+}
