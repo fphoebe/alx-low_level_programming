92eb7b83c3d19b39bcf9ba03421ad138aad2a518
fphoebe <fphoebe36@gmail.com>
2023-03-24 14:09:07 -0700

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,16 @@
    *@x: the number to be checked
    *Return: 1 for the upper letter or 0 for any else
    */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
 int _isupper(int n)
 {
 	if (x >= 65 && x <= 90)
