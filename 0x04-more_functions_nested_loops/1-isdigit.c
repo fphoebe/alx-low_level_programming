a5983f146b329d77465200cbed9e678729013ba9
fphoebe <fphoebe36@gmail.com>
2023-03-24 19:36:23 -0700

    a

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+/**
+ * print - _isdigit - A program that prints the last digit
+ * of a number.
+ *
+ * @n: input number as an integer.
+ *
+ * Return:is digit.
+ */
+int _isdigit(int i)
+{
+	int i;
+
+	if (i >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
