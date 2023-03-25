da08672751938b9f5685adbbc5722b55b4965326
fphoebe <fphoebe36@gmail.com>
2023-03-24 19:55:01 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *main -entry point 
+  *int-mul - prints two integers multipying
+  *Return -always 0
+  */
+int mul(int a, int b)
+{
+	int a;
+	int b;
+	int mul;
+
+	for (mul = a*b)
+	{
+		_putchar(mul);
+	
+	}
+	return (0)
+}
