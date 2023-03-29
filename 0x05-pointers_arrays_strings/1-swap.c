da85f3ede39c710dd133504b5ff5d23aa6d2af3e
fphoebe <fphoebe36@gmail.com>
2023-03-28 20:11:17 -0700

    a

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   *swap_int -swaps the value of two integers
   *@a: int to swap
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,6 +12,6 @@ int _strlen(char *s)
 
 	while (*s +count != '\0')
 		count++;
-	}
+
 	return (count);
 }
