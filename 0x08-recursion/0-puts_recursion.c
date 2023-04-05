22a30d62bd656e57246b6428282318f488c2a8f9
fphoebe <fphoebe36@gmail.com>
2023-04-05 13:06:19 -0700

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+ #include "main.h"
+   /**
+     *main - puts recursion
+     *
+     *Return: always 0
+     */
+int main(void)
+	{
+           _puts_recursion("char *s");
+          return (0);
+	}
