f58e5788cd2cc6c6095c3350dde8787452bb279e
fphoebe <fphoebe36@gmail.com>
2023-04-05 13:31:32 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,8 +4,18 @@
      *
      *Return: always 0
      */
-int main(void)
-	{
-           _puts_recursion("char *s");
-          return (0);
-	}
+char _puts_recursion factorial(char *s)
+{
+	if (*s == 0)
+	return 0;
+}
+if (*s == 1)
+{ return 1
+}
+return *s * factorial(n-1);
+}
+int main()
+{
+	putchar("%c\n", factorial ());
+	return (0);
+}
